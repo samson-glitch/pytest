b19f27d323e066702d8a3844650096983b4a3d47
Inakoju Samson <samminakoju@gmail.com>
2023-07-12 16:52:29 +0100

    update source file and testing file

--- hello.py
+++ hello.py
@@ -1,5 +1,2 @@
 def more_hello():
     return "hi"
-
-def more_goodbye():
-    return 'bye'
--- test_hello.py
+++ test_hello.py
@@ -2,6 +2,3 @@ from hello import more_hello
 
 def test_more_hello():
     assert "hi" == more_hello()
-
-def test_more_hello2():
-    assert "bye" == more_hello()
