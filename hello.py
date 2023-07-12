fe6c77c46c3ac06a8e97cd59e499a4dbb322ffea
Inakoju Samson <samminakoju@gmail.com>
2023-07-12 16:25:25 +0100

    Created source file and testing file

---
+++ hello.py
@@ -0,0 +1,5 @@
+def more_hello():
+    return "hi"
+
+def more_goodbye():
+    return 'bye'
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+from hello import more_hello
+
+def test_more_hello():
+    assert "hi" == more_hello()
+
+def test_more_hello2():
+    assert "bye" == more_hello()
